11c16311f0a4f9a3b6d0f8686188830531861c00
NashLegend <panzhihuipzh@gmail.com>
2014-02-14 10:37:20 +0800

    SlidingMenu

---
+++ src/com/example/customalarm/LeftMenu.java
@@ -0,0 +1,24 @@
+package com.example.customalarm;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class LeftMenu extends Fragment {
+
+	public LeftMenu() {
+		// TODO 自动生成的构造函数存根
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// TODO 自动生成的方法存根
+		View view = inflater.inflate(R.layout.menu_left, null);
+
+		return view;
+	}
+
+}
