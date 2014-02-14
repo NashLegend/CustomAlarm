7c97e0ee2fd65b2d1d2d6ab3ce023b5f072cbba0
NashLegend <panzhihuipzh@gmail.com>
2014-02-14 10:36:48 +0800

    SlidingMenu

--- gen/com/example/customalarm/R.java
+++ gen/com/example/customalarm/R.java
@@ -24,11 +24,18 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int LeftMenu=0x7f080002;
+        public static final int MainContent=0x7f080001;
+        public static final int RightMenu=0x7f080003;
+        public static final int action_settings=0x7f080004;
+        public static final int content=0x7f080000;
     }
     public static final class layout {
         public static final int activity_alarm_detail=0x7f030000;
         public static final int activity_main=0x7f030001;
+        public static final int content_main=0x7f030002;
+        public static final int menu_left=0x7f030003;
+        public static final int menu_right=0x7f030004;
     }
     public static final class menu {
         public static final int alarm_detail=0x7f070000;
--- src/com/example/customalarm/MainActivity.java
+++ src/com/example/customalarm/MainActivity.java
@@ -2,18 +2,25 @@ package com.example.customalarm;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity {
 
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		Alarm.startAlarmRestore(getApplicationContext());
+		initSlidingMenu();
 	}
 
-
+	private void initSlidingMenu() {
+		getSupportFragmentManager().beginTransaction()
+				.replace(R.id.content, new MainContent()).commit();
+		
+	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
