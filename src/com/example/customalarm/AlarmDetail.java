a9cb8075731a820c5a0f09970c10a0df84dc11e9
NashLegend <panzhihuipzh@gmail.com>
2014-03-18 09:42:12 +0800

    ttt

--- src/com/example/customalarm/AlarmDetail.java
+++ src/com/example/customalarm/AlarmDetail.java
@@ -11,7 +11,7 @@ public class AlarmDetail extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+		super.onCreate(savedInstanceState); 
 		setContentView(R.layout.activity_alarm_detail);
 	}
 
