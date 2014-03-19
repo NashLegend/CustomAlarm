fb9f10b00d410d1e2b0071b56652eb069c28f2c4
NashLegend <panzhihuipzh@gmail.com>
2014-03-19 22:42:09 +0800

    Weekly&Yearly Left

--- src/com/example/customalarm/MainActivity.java
+++ src/com/example/customalarm/MainActivity.java
@@ -1,4 +1,3 @@
-
 package com.example.customalarm;
 
 import java.util.ArrayList;
@@ -26,132 +25,134 @@ import android.widget.ListView;
  * @author NashLegend I put this as minority
  */
 public class MainActivity extends Activity {
-    private ActionBar mActionBar;
-    private MyAlarmAdapter adapter;
-    private ListView listView;
-    private ImageView imageView;
+	private ActionBar mActionBar;
+	private MyAlarmAdapter adapter;
+	private ListView listView;
+	private ImageView imageView;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Alarm.startAlarmRestore(getApplicationContext());
-        initView();
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		Alarm.startAlarmRestore(getApplicationContext());
+		initView();
+	}
 
-    private void initView() {
-        mActionBar = getActionBar();
-        mActionBar.setDisplayShowHomeEnabled(true);
-        imageView = (ImageView) findViewById(R.id.my_clarm_bg_img);
-        listView = (ListView) findViewById(R.id.MyAlarmList);
-        adapter = new MyAlarmAdapter(this);
-        listView.setAdapter(adapter);
-        buildAlarmList();
-    }
+	private void initView() {
+		mActionBar = getActionBar();
+		mActionBar.setDisplayShowHomeEnabled(true);
+		imageView = (ImageView) findViewById(R.id.my_clarm_bg_img);
+		listView = (ListView) findViewById(R.id.MyAlarmList);
+		adapter = new MyAlarmAdapter(this);
+		listView.setAdapter(adapter);
+		buildAlarmList();
+	}
 
-    public void buildAlarmList() {
-        Alarm todayAlarm = new Alarm(this, null);
-        todayAlarm.setIsSplitter(true);
-        todayAlarm.setTag("Today");
-        todayAlarm.setRemark("weather");
+	public void buildAlarmList() {
+		Alarm todayAlarm = new Alarm(this, null);
+		todayAlarm.setIsSplitter(true);
+		todayAlarm.setTag("Today");
+		todayAlarm.setRemark("weather");
 
-        ArrayList<Alarm> alarms = getAllAlarms(this);
-        if (alarms.size() == 0) {
-            imageView.setVisibility(View.VISIBLE);
-            listView.setVisibility(View.GONE);
-        } else {
-            imageView.setVisibility(View.GONE);
-            listView.setVisibility(View.VISIBLE);
-            AlarmComparator alarmComparator = new AlarmComparator();
-            Collections.sort(alarms, alarmComparator);
+		ArrayList<Alarm> alarms = getAllAlarms(this);
+		if (alarms.size() == 0) {
+			imageView.setVisibility(View.VISIBLE);
+			listView.setVisibility(View.GONE);
+		} else {
+			imageView.setVisibility(View.GONE);
+			listView.setVisibility(View.VISIBLE);
+			AlarmComparator alarmComparator = new AlarmComparator();
+			Collections.sort(alarms, alarmComparator);
 
-            alarms.add(0, todayAlarm);
+			alarms.add(0, todayAlarm);
 
-            int tmpNum = 0;
-            int currentSplitterType = alarms.get(1).getSpanType();
-            Alarm currentSplitterAlarm = todayAlarm;
-            Alarm alarm2 = null;
-            for (int i = 1; i < alarms.size(); i++) {
-                alarm2 = alarms.get(i);
-                if (alarm2.getSpanType() == currentSplitterType) {
-                    tmpNum++;
-                } else {
-                    if (tmpNum == 0) {
-                        alarms.remove(currentSplitterAlarm);
-                        i--;
-                    } else {
-                        currentSplitterAlarm.setType(currentSplitterType);
-                        currentSplitterAlarm.setTag(MyAlarmSplitter
-                                .type2Date(currentSplitterType));
-                        currentSplitterAlarm.setNumInSplitter(tmpNum);
-                        currentSplitterAlarm.setRemark(":" + tmpNum + "个");
-                    }
+			int tmpNum = 0;
+			int currentSplitterType = alarms.get(1).getSpanType();
+			Alarm currentSplitterAlarm = todayAlarm;
+			Alarm alarm2 = null;
+			for (int i = 1; i < alarms.size(); i++) {
+				alarm2 = alarms.get(i);
+				slog(alarm2.getSpanType() + "");
+				if (alarm2.getSpanType() == currentSplitterType) {
+					tmpNum++;
+				} else {
+					if (tmpNum == 0) {
+						alarms.remove(currentSplitterAlarm);
+						i--;
+					} else {
+						currentSplitterAlarm.setType(currentSplitterType);
+						currentSplitterAlarm.setTag(MyAlarmSplitter
+								.type2Date(currentSplitterType));
+						currentSplitterAlarm.setNumInSplitter(tmpNum);
+						currentSplitterAlarm.setRemark(":" + tmpNum + "个");
+					}
 
-                    tmpNum = 1;
-                    Alarm alarm3 = new Alarm(this, null);
-                    alarm3.setIsSplitter(true);
-                    currentSplitterAlarm = alarm3;
-                    alarms.add(i, currentSplitterAlarm);
-                    currentSplitterType = alarm2.getSpanType();
-                    i++;
-                }
-            }
+					tmpNum = 1;
+					Alarm alarm3 = new Alarm(this, null);
+					alarm3.setIsSplitter(true);
+					currentSplitterAlarm = alarm3;
+					alarms.add(i, currentSplitterAlarm);
+					currentSplitterType = alarm2.getSpanType();
+					i++;
+				}
+			}
 
-            if (tmpNum == 0) {
-                alarms.remove(currentSplitterAlarm);
-            } else {
-                currentSplitterAlarm.setType(currentSplitterType);
-                currentSplitterAlarm.setTag(MyAlarmSplitter.type2Date(currentSplitterType));
-                currentSplitterAlarm.setNumInSplitter(tmpNum);
-                currentSplitterAlarm.setRemark(":" + tmpNum + "个");
-            }
-            adapter.setList(alarms);
-            adapter.notifyDataSetChanged();
-        }
-    }
+			if (tmpNum == 0) {
+				alarms.remove(currentSplitterAlarm);
+			} else {
+				currentSplitterAlarm.setType(currentSplitterType);
+				currentSplitterAlarm.setTag(MyAlarmSplitter
+						.type2Date(currentSplitterType));
+				currentSplitterAlarm.setNumInSplitter(tmpNum);
+				currentSplitterAlarm.setRemark(":" + tmpNum + "个");
+			}
+			adapter.setList(alarms);
+			adapter.notifyDataSetChanged();
+		}
+	}
 
-    private ArrayList<Alarm> getAllAlarms(Context context) {
-        AlarmHelper helper = new AlarmHelper(context);
-        return helper.getAlarms();
-    }
+	private ArrayList<Alarm> getAllAlarms(Context context) {
+		AlarmHelper helper = new AlarmHelper(context);
+		return helper.getAlarms();
+	}
 
-    public class AlarmComparator implements Comparator<Alarm> {
+	public class AlarmComparator implements Comparator<Alarm> {
 
-        @Override
-        public int compare(Alarm lhs, Alarm rhs) {
-            Long com = lhs.getNextRingSpan() - rhs.getNextRingSpan();
-            if (com > 0) {
-                return 1;
-            } else if (com == 0) {
-                return 0;
-            } else {
-                return -1;
-            }
+		@Override
+		public int compare(Alarm lhs, Alarm rhs) {
+			Long com = lhs.getNextRingSpan() - rhs.getNextRingSpan();
+			if (com > 0) {
+				return 1;
+			} else if (com == 0) {
+				return 0;
+			} else {
+				return -1;
+			}
 
-        }
-    }
+		}
+	}
 
-    private void slog(String str) {
-        Log.i("cus", str + "__");
-    }
+	private void slog(String str) {
+		Log.i("cus", str + "__");
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // TODO 自动生成的方法存根
-        switch (item.getItemId()) {
-            case R.id.alarm_set:
-                Intent intent = new Intent(this, SetAlarmActivity.class);
-                startActivity(intent);
-                break;
-            default:
-                break;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// TODO 自动生成的方法存根
+		switch (item.getItemId()) {
+		case R.id.alarm_set:
+			Intent intent = new Intent(this, SetAlarmActivity.class);
+			startActivity(intent);
+			break;
+		default:
+			break;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 }
--- src/com/example/customalarm/fragments/SetMonthlyAlarmFragment.java
+++ src/com/example/customalarm/fragments/SetMonthlyAlarmFragment.java
@@ -1,22 +1,125 @@
 package com.example.customalarm.fragments;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.UUID;
+
+import android.annotation.SuppressLint;
+import android.app.TimePickerDialog;
+import android.app.TimePickerDialog.OnTimeSetListener;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TimePicker;
 
 import com.example.customalarm.R;
+import com.example.customalarm.core.Alarm;
+import com.example.customalarm.ui.NumberPickerDialog;
 
+@SuppressLint("SimpleDateFormat")
 public class SetMonthlyAlarmFragment extends BaseSetAlarmFragment {
+	private Button dayButton;
+	private Button timeButton;
 
-    public SetMonthlyAlarmFragment() {
-        // TODO 自动生成的构造函数存根
-    }
-    
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        view=inflater.inflate(R.layout.fragment_set_monthly_alarm, null);
-        return view;
-    }
+	public SetMonthlyAlarmFragment() {
+		// TODO 自动生成的构造函数存根
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		QCalendar = new GregorianCalendar();
+		QCalendar.set(Calendar.MONTH, Calendar.JANUARY);// 设为一月则可接收所有日期
+		QCalendar.set(Calendar.DAY_OF_MONTH, 1);
+		QCalendar.set(Calendar.HOUR_OF_DAY, 8);
+		QCalendar.set(Calendar.MINUTE, 0);
+		QCalendar.set(Calendar.MILLISECOND, 0);
+
+		view = inflater.inflate(R.layout.fragment_set_monthly_alarm, null);
+		tagView = (EditText) view.findViewById(R.id.TagInput);
+		dayButton = (Button) view.findViewById(R.id.daysofmonth);
+		timeButton = (Button) view.findViewById(R.id.timeButton);
+
+		dayButton.setOnClickListener(this);
+		timeButton.setOnClickListener(this);
+
+		dayButton.setText("1");
+
+		DateFormat df = new SimpleDateFormat("HH:mm");
+		String timeString = df.format(QCalendar.getTime());
+		timeButton.setText(timeString);
+
+		return view;
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.daysofmonth:
+			NumberPickerDialog.Builder builder = new NumberPickerDialog.Builder(
+					getActivity());
+			NumberPickerDialog dialog1 = builder
+					.setNumberSetListener(
+							new NumberPickerDialog.onNumberSetListener() {
+
+								@Override
+								public void onNumberSet(DialogInterface dialog,
+										int num) {
+									dayButton.setText(String.valueOf(num));
+									QCalendar.set(Calendar.DAY_OF_MONTH, num);
+									dialog.dismiss();
+								}
+							}).setMaxValue(72).setMinValue(0).create();
+			dialog1.show();
+			break;
+		case R.id.timeButton:
+			new TimePickerDialog(getActivity(), new OnTimeSetListener() {
+				@Override
+				public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+					QCalendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
+					QCalendar.set(Calendar.MINUTE, minute);
+					DateFormat df = new SimpleDateFormat("HH:mm");
+					String timeString = df.format(QCalendar.getTime());
+					timeButton.setText(timeString);
+				}
+			}, QCalendar.get(Calendar.HOUR_OF_DAY), QCalendar
+					.get(Calendar.MINUTE), true).show();
+			break;
+
+		default:
+			break;
+		}
+		super.onClick(v);
+	}
+
+	@Override
+	public void saveAlarm() {
+		Bundle bundle = new Bundle();
+		String id = UUID.randomUUID().toString().replaceAll("-", "");
+		bundle.putString(Alarm.ALARM_ID, UUID.randomUUID().toString());
+		bundle.putString(Alarm.ALARM_GROUP_ID, id);
+		bundle.putInt(Alarm.ALARM_TYPE, Alarm.ALARM_MONTHLY);
+		bundle.putBoolean(Alarm.ALARM_CANCELABLE, true);
+		String tmptagString = tagView.getText().toString().trim();
+		if (tmptagString.equals("")) {
+			tmptagString = getResources().getString(R.string.Instant_Alarm);
+		}
+		bundle.putString(Alarm.ALARM_TAG, tmptagString);
+		bundle.putSerializable(Alarm.ALARM_CALENDAR, QCalendar);
+		bundle.putIntArray(Alarm.ALARM_DAYS_OF_SOME, null);
+		bundle.putBoolean(Alarm.ALARM_AVAILABLE, true);
+		bundle.putString(Alarm.ALARM_REMARK, "");
+		bundle.putString(Alarm.ALARM_IMAGE, null);
+		bundle.putString(Alarm.ALARM_GROUP_NAME, "");
+		Alarm alarm = new Alarm(getActivity().getApplicationContext(), bundle);
+		alarm.activate();
+		alarm.storeInDB();
+	}
 
 }
